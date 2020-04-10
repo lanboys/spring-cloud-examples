21748cb8adb076aa1cebd37b2697ec4857a04e88
lanboys <lan_bing2013@163.com>
2020-04-10 17:33:48 +0800

    springcloud(四)：熔断器Hystrix

--- spring-cloud-hystrix/spring-cloud-consumer/src/main/java/com/neo/remote/HelloRemoteHystrix.java
+++ spring-cloud-hystrix/spring-cloud-consumer/src/main/java/com/neo/remote/HelloRemoteHystrix.java
@@ -1,6 +1,5 @@
 package com.neo.remote;
 
-import com.neo.remote.HelloRemote;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,6 +11,6 @@ public class HelloRemoteHystrix implements HelloRemote{
 
     @Override
     public String hello(@RequestParam(value = "name") String name) {
-        return "hello " +name+", this messge send failed ";
+        return "hello " +name+", this message send failed ";
     }
 }
--- spring-cloud-hystrix/spring-cloud-producer/src/main/java/com/neo/ProducerApplication.java
+++ spring-cloud-hystrix/spring-cloud-producer/src/main/java/com/neo/ProducerApplication.java
@@ -2,13 +2,14 @@ package com.neo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
+//@EnableDiscoveryClient
 public class ProducerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProducerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
 }
