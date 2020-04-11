2b63e5ae8ab61148b81416038e18f9d4282ce4a3
lanboys <lan_bing2013@163.com>
2020-04-11 14:25:52 +0800

    springcloud(五)：熔断监控Hystrix Dashboard和Turbine

--- hystrix-dashboard-turbine/spring-cloud-consumer-hystrix-dashboard/src/main/java/com/neo/ConsumerApplication.java
+++ hystrix-dashboard-turbine/spring-cloud-consumer-hystrix-dashboard/src/main/java/com/neo/ConsumerApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
@@ -13,6 +12,9 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableFeignClients
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+// 开启 EnableHystrixDashboard 后, 才能访问 http://xxxx:xx/hystrix, 打开进入断路器App监控页面的引导页
+// 开启 EnableTurbine 后, 才能访问 http://xxxx:xx/turbine.stream, 进行 ping 多个断路器App数据
+// 开启 EnableCircuitBreaker 后, 才能访问 http://xxxx:xx/hystrix.stream , 进行 ping 单个断路器App数据
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
