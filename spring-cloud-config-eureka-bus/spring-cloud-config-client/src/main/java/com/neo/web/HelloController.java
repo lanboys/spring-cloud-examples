e07662eed8fb95cd2c2bc580dbbb52b7baceee73
lanboys <lan_bing2013@163.com>
2020-04-13 17:24:34 +0800

    springcloud(九)：配置中心和消息总线（配置中心终结版）

--- spring-cloud-config-eureka-bus/spring-cloud-config-client/src/main/java/com/neo/web/HelloController.java
+++ spring-cloud-config-eureka-bus/spring-cloud-config-client/src/main/java/com/neo/web/HelloController.java
@@ -1,13 +1,11 @@
 package com.neo.web;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope
-class HelloController {
+public class HelloController {
 
     @Value("${neo.hello}")
     private String hello;
