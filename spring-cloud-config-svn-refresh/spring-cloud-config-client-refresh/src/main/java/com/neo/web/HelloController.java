d4a024ba9b0c1d20ebe15a679bde96e6e9731b0a
lanboys <lan_bing2013@163.com>
2020-04-13 12:00:43 +0800

    springcloud(七)：配置中心svn示例和refresh

--- spring-cloud-config-svn-refresh/spring-cloud-config-client-refresh/src/main/java/com/neo/web/HelloController.java
+++ spring-cloud-config-svn-refresh/spring-cloud-config-client-refresh/src/main/java/com/neo/web/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
-class HelloController {
+public class HelloController {
 
     @Value("${neo.hello}")
     private String hello;
