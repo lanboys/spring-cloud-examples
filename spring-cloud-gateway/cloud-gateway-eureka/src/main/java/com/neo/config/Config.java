faf7e33b5ecb23ab4a9300a177d6a765bda0b7f7
lanboys <lan_bing2013@163.com>
2020-04-16 11:30:43 +0800

    springcloud(十七)：服务网关 Spring Cloud GateWay 熔断、限流、重试

--- spring-cloud-gateway/cloud-gateway-eureka/src/main/java/com/neo/config/Config.java
+++ spring-cloud-gateway/cloud-gateway-eureka/src/main/java/com/neo/config/Config.java
@@ -2,15 +2,17 @@ package com.neo.config;
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import reactor.core.publisher.Mono;
 
+@Configuration
 public class Config {
 
     @Bean
     public KeyResolver ipKeyResolver() {
         return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
     }
-
 
     @Bean
     KeyResolver userKeyResolver() {
